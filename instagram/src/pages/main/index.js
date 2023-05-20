9d04f7bfef2f01c0ed3808800372627490d0dfa8
jeongwooHam <hamjw0122@gmail.com>
2023-05-20 19:18:08 +0900

    main 페이지 구현

--- instagram/src/pages/main/index.js
+++ instagram/src/pages/main/index.js
@@ -1,11 +1,43 @@
+import PostList from "./components/post-list";
+import RecommendUsers from "./components/recommend-users";
+
+import styled from "styled-components";
 import Story from "./components/story";
 
 const Main = () => {
   return (
     <>
-      <Story />
+      <S.Container>
+        <S.Contents>
+          <Story /> <PostList />
+        </S.Contents>
+        <S.Footer>
+          <RecommendUsers />
+        </S.Footer>
+      </S.Container>
     </>
   );
 };
-
 export default Main;
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+`;
+
+const Contents = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Footer = styled.div`
+  width: 30%;
+`;
+
+const S = {
+  Container,
+  Contents,
+  Footer,
+};
