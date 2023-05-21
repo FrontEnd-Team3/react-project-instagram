4a91dc221ba8494294b87f4a4e7609e271bf77e4
jeongwooHam <hamjw0122@gmail.com>
2023-05-21 11:04:00 +0900

    '버그수정''

--- instagram/src/pages/main/components/story.js
+++ instagram/src/pages/main/components/story.js
@@ -13,6 +13,16 @@ const Story = () => {
         <img src="img/moon7.jpeg" />
         <img src="img/moon8.jpeg" />
       </ImageBox>
+      <TextBox>
+        <p>moon1</p>
+        <p>moon2</p>
+        <p>moon3</p>
+        <p>moon4</p>
+        <p>moon5</p>
+        <p>moon6</p>
+        <p>moon7</p>
+        <p>moon8</p>
+      </TextBox>
     </>
   );
 };
@@ -20,14 +30,27 @@ export default Story;
 
 const ImageBox = styled.div`
   display: flex;
-
   align-items: center;
   flex-direction: row;
-
+  margin-top: 30px;
   img {
-    width: 60px;
-    height: 60px;
+    width: 55px;
+    height: 55px;
     border-radius: 70%;
     overflow: hidden;
+    margin-left: 20px;
+    cursor: pointer;
+  }
+`;
+const TextBox = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  font-size: 10px;
+  margin-bottom: 40px;
+  margin-right: 24px;
+  p {
+    margin-left: 41.5px;
+    font-weight: 500;
   }
 `;
